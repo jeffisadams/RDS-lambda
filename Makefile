00d4aabb630e0de77d606759816bb2a7c4de323d
Jeff Adams <jeffisadams@gmail.com>
2019-07-02 13:57:24 -0400

    Adding the guts. Incomplete.

---
+++ Makefile
@@ -0,0 +1,52 @@
+ifeq ($(STACK_NAME),)
+STACK_NAME := 'RDS-lambda-test-stack'
+endif
+
+ifeq ($(STACK_BUCKET),)
+STACK_BUCKET := 'RDS-test-stack-plumbing-bucket'
+endif
+
+
+.PHONY: test
+test:
+	aws cloudformation validate-template --template-body file://template.yaml
+
+.PHONY: clean
+clean:
+	rm -rf ./dist
+	rm -rf template_deploy.yaml
+
+.PHONY: deps
+deps: clean
+	go get github.com/aws/aws-lambda-go/events
+	go get github.com/aws/aws-lambda-go/lambda
+	go get github.com/aws/aws-sdk-go/aws/session
+	go get github.com/jmoiron/sqlx
+	go get github.com/go-sql-driver/mysql
+
+.PHONY: build
+build: deps
+	GOOS=linux go build -o dist/main ./src/main.go
+
+# Leaving this here as a tidbit of extra info, but it's not 
+# .PHONY: api
+# api: build
+# 	sam local start-api --env-vars env.json
+
+.PHONY: deploy
+deploy:
+	aws cloudformation package \
+		--template-file template.yaml \
+		--output-template template_deploy.yaml \
+		--s3-bucket $(STACK_BUCKET)
+
+	aws cloudformation deploy \
+		--no-fail-on-empty-changeset \
+		--template-file template_deploy.yaml \
+		--stack-name test-database-service \
+		--parameter-overrides "Env=dev" "RDSDatabaseUserPassword=TestDatabasePasswordOfPassing"
+
+.PHONY: teardown
+teardown:
+	aws cloudformation delete-stack --stack-name test-database-service
+	clean
---
+++ src/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+var dbName = os.Getenv("DB_NAME")
+var dbHost = os.Getenv("DB_HOST")
+var dbPort = os.Getenv("DB_PORT")
+var dbServiceUser = os.Getenv("DB_SERVICE_USER")
+var dbAdminUser = "admin"
+var dbPassword = os.Getenv("DB_PASSWORD")
+
+// Load the Env variables
+var db *sqlx.DB
+
+var userTable = `
+CREATE TABLE IF NOT EXISTS user(
+	id INT NOT NULL AUTO_INCREMENT,
+	email varchar(255) DEFAULT NULL,
+	phone varchar(10) DEFAULT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT UNIQUE(email),
+	CONSTRAINT UNIQUE(phone)
+);
+`
+
+// handleRequest Sends out the MMS Message using the Twilio Service
+func handleCrudRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Actual function start")
+	db.MustExec(`CREATE DATABASE IF NOT EXISTS ?`, dbName)
+	// db.MustExec(fmt.Sprintf(`USE %s`, dbName))
+
+	// db.MustExec(fmt.Sprintf("CREATE USER '%s' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';", dbServiceUser))
+	// db.MustExec(fmt.Sprintf("GRANT ALL ON PRIVILEDGES ON %s.* TO '%s'@'%';", dbName, dbServiceUser))
+	// db.MustExec("FLUSH PRIVILEDGES;")
+
+	db.MustExec(userTable)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello there",
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "*",
+			"Access-Control-Allow-Headers": "*",
+		},
+	}, nil
+}
+
+func main() {
+	lambda.Start(handleCrudRequest)
+}
+
+func init() {
+	// Login to the DB
+	connectionStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/?timeout=10s", dbAdminUser, dbPassword, dbHost, dbPort)
+
+	var err error
+	db, err = sqlx.Connect("mysql", connectionStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
